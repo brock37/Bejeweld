842e71c5a241aece12f9f8332eb0ed1303a2fd79
Brock37 <remondflorent@gmail.com>
2019-01-09 15:01:35 +0100

    Ajout d'une classe Engine

---
+++ include/Engine.h
@@ -0,0 +1,30 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include <SFML/Graphics.hpp>
+
+#include <vector>
+
+#include "Texture_manager.h"
+/* Engine - Charge SFML
+ * Est chargé de gerer l'affichage de l'ecran
+ */
+
+class Engine
+{
+    public:
+        ///Ctr initialise SFML - icone - titre
+        Engine();
+        virtual ~Engine();
+
+        /// Fonction principal - lance le game loop
+        void runEngine();
+
+    protected:
+
+    private:
+    sf::RenderWindow m_window;
+    Texture_manager m_textureManager;
+};
+
+#endif // ENGINE_H
---
+++ src/Engine.cpp
@@ -0,0 +1,29 @@
+#include "Engine.h"
+
+Engine::Engine()
+{
+    //ctor
+    ///Creation de la fenetre
+    sf::RenderWindow m_window(sf::VideoMode(500, 400), "Bejeweld");
+
+    ///Remplissage du manager de texture
+    Texture_manager m_textureManager;
+    m_textureManager.ajouterTexture("ressources/Images/background.png");
+    m_textureManager.ajouterTexture("ressources/Images/tileset.png");
+    m_textureManager.ajouterTexture("ressources/Images/food.png");
+    m_textureManager.ajouterTexture("ressources/Images/wood.png");
+    m_textureManager.ajouterTexture("ressources/Images/gold.png");
+
+
+    ///Initialisation du(des) screen(s)
+}
+
+Engine::~Engine()
+{
+    //dtor
+}
+
+void Engine::runEngine()
+{
+
+}//Fin de la fonction run
