edf556d2dcf5ba7cec90152579b93656b7d449c6
Brock37 <remondflorent@gmail.com>
2019-01-24 11:50:10 +0100

    Strat implementation Run function with simple event catch and display board

--- src/GameScreen.cpp
+++ src/GameScreen.cpp
@@ -14,5 +14,37 @@ GameScreen::~GameScreen()
 
 int GameScreen::Run(sf::RenderWindow &App)
 {
+    bool running= true;
+    sf::Event Event;
+    
+    while(running)
+    {
+        //Verifying events
+		while (App.pollEvent(Event))
+		{
+			// Window closed
+			if (Event.type == sf::Event::Closed)
+			{
+				return (-1);
+			}
+			//Key pressed
+			if (Event.type == sf::Event::KeyPressed)
+			{
+				switch (Event.key.code)
+				{
+				case sf::Keyboard::Escape:
+					return (0);
+					break;
+				default:
+					break;
+				}
+			}
+        }
+        
+        App.clear();
+        App.draw(m_boardView);
+        App.display();
+    }
+    
     return -1;
 }
