95ae3470d9be48e2de71b6abc1b55f373834b688
Brock37 <remondflorent@gmail.com>
2019-01-24 13:14:29 +0100

    Implement draw Item function

--- src/BoardView.cpp
+++ src/BoardView.cpp
@@ -13,5 +13,12 @@ BoardView::~BoardView()
 
 void BoardView::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
+    for(int i(0); i < m_boardModel.getNbRows(); i++)
+    {
+        for(int j(0); j < m_boardModel.getNbCols(); j++)
+        {
+            m_boardModel.getItem(i,j).draw(target, states);
+        }
+    }
 
 }
